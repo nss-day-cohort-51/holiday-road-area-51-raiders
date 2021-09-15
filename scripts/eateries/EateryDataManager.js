2d9748443d5fb96bcd6b92195da479c4b82d1f06
Tia Thomason <88596394+TT-615@users.noreply.github.com>
2021-09-15 15:50:02 -0500

    tt-weatheralertjava

--- scripts/eateries/EateryDataManager.js
+++ scripts/eateries/EateryDataManager.js
@@ -1,7 +1,22 @@
+let  allEateries = []
+
 export const getEateries = () => {
     return fetch("http://holidayroad.nss.team/eateries")
     .then(response => response.json())
     .then(parsedResponse => {
-        return parsedResponse
+        allEateries = parsedResponse;
+        return parsedResponse;
     })
 }
+export const getSingleEatery = (id) => {
+    return fetch(`http://holidayroad.nss.team/eateries/${id}`)
+    .then(response => response.json())
+    .then(parsedResponse => {
+        return parsedResponse;
+    })
+}
+    
+
+    export const getEateryArray = () => {
+        return allEateries;
+    }
--- scripts/parks/ParkDataManager.js
+++ scripts/parks/ParkDataManager.js
@@ -24,6 +24,7 @@ export const getParks = (stateCode) => {
     })
 }
 
+
 export const getParkArray = () => {
     return allParks;
 }
